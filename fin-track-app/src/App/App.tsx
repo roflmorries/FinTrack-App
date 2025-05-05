249393d5b92fc7218fa22fdd960fc829fa95a801
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-05 22:58:15 +0300

    add routing

--- fin-track-app/src/App/App.tsx
+++ fin-track-app/src/App/App.tsx
@@ -1,10 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
 import '../shared/ui/App.css'
+import AppRoutes from './providers/router/AppRoutes'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   )
 }
 
