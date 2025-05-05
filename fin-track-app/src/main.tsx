145c8301d4022ce565b33f254232ff7e671a9e8e
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-05 22:56:00 +0300

    update main

--- fin-track-app/src/main.tsx
+++ fin-track-app/src/main.tsx
@@ -1,7 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app/App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './app/store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
