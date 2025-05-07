192b8d1589cf21879340a74e5c65ea8a0f30d4e5
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-07 22:20:29 +0300

    update app

--- fin-track-app/src/App/App.tsx
+++ fin-track-app/src/App/App.tsx
@@ -1,8 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
 import '../shared/ui/App.css'
 import AppRoutes from './providers/router/AppRoutes'
+import { useAppDispatch } from '../shared/lib/hooks/redux/reduxTypes'
+import { checkAuth } from '../entities/user/model/userThunks';
+import { useEffect } from 'react';
 
 function App() {
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(checkAuth());
+  }, []);
 
   return (
     <BrowserRouter>
