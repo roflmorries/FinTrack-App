af310219a174536867b79e92eda9afc81ff5d5fe
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-07 22:21:55 +0300

    update home page

--- fin-track-app/src/pages/HomePage/HomePage.tsx
+++ fin-track-app/src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,17 @@
 import { Button } from "antd"
 import { useNavigate } from "react-router-dom"
+import { useAppSelector } from "../../shared/lib/hooks/redux/reduxTypes";
+import { useEffect } from "react";
 
 export default function HomePage() {
   const navigate = useNavigate();
+  const { isAuth} = useAppSelector((state) => state.user)
+  
+    useEffect(() => {
+      if (isAuth) {
+        navigate('/dashboard')
+      }
+    }, [isAuth, navigate])
   return (
     <>
       <h1>HELLO THERE</h1>
