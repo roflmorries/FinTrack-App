d106ccfb81aaa1f516adcaaef676185526f55e44
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-09 21:00:44 +0300

    update app

--- fin-track-app/src/App/App.tsx
+++ fin-track-app/src/App/App.tsx
@@ -2,16 +2,25 @@ import { BrowserRouter } from 'react-router-dom'
 import '../shared/ui/App.css'
 import AppRoutes from './providers/router/AppRoutes'
 import { useAppDispatch } from '../shared/lib/hooks/redux/reduxTypes'
-import { checkAuth } from '../entities/user/model/userThunks';
 import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { fetchUserData } from '../entities/user/model/userThunks';
+import { auth } from '../shared/config/firebase';
 
 function App() {
 
   const dispatch = useAppDispatch();
 
-  useEffect(() => {
-    dispatch(checkAuth());
-  }, []);
+  // useEffect(() => {
+  //   const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+  //     if (firebaseUser) {
+  //       dispatch(fetchUserData(firebaseUser.uid));
+  //     }
+  //   });
+
+  //   return () => unsubscribe();
+  // }, [dispatch]);
+
 
   return (
     <BrowserRouter>
