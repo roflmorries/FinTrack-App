4f297a8ba080d1a30dc956e174d930e7b19c7bc5
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-11 00:15:13 +0300

    update button

--- fin-track-app/src/features/auth/ui/GoogleLoginButton.tsx
+++ fin-track-app/src/features/auth/ui/GoogleLoginButton.tsx
@@ -1,26 +1,26 @@
-// import { Button } from "antd";
-// import { useAppDispatch } from "../../../shared/lib/hooks/redux/reduxTypes";
-// import { signInUserWithGoogle } from "../../../entities/user/model/userThunks";
-// import { useNavigate } from "react-router-dom";
+import { Button } from "antd";
+import { useAppDispatch } from "../../../shared/lib/hooks/redux/reduxTypes";
+import { signInUserWithGoogle } from "../../../entities/user/model/userThunks";
+import { useNavigate } from "react-router-dom";
 
-// export default function GoogleLoginButton() {
-//   const dispatch = useAppDispatch();
-//   const navigate = useNavigate();
+export default function GoogleLoginButton() {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
 
-//   const handleLoginByGoogle = async () => {
-//     try {
-//       const resultAction = await dispatch(signInUserWithGoogle());
-//       if(signInUserWithGoogle.fulfilled.match(resultAction)){
-//         navigate('/dashboard')
-//       } else {
-//         console.error('Login error:', resultAction.payload)
-//       }
-//     } catch(error) {
-//       console.error('Google auth error:', error);
-//     }
-//   }
+  const handleLoginByGoogle = async () => {
+    try {
+      const resultAction = await dispatch(signInUserWithGoogle());
+      if(signInUserWithGoogle.fulfilled.match(resultAction)){
+        navigate('/dashboard')
+      } else {
+        console.error('Login error:', resultAction.payload)
+      }
+    } catch(error) {
+      console.error('Google auth error:', error);
+    }
+  }
 
-//   return (
-//     <Button onClick={handleLoginByGoogle} >Sign in With Google</Button>
-//   )
-// }
+  return (
+    <Button onClick={handleLoginByGoogle} >Sign in With Google</Button>
+  )
+}
