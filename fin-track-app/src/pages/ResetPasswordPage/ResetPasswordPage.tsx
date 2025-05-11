2263267fb12adc628d8fec6f933772dcd28a4d6d
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-11 17:29:25 +0300

    add reset password

--- fin-track-app/src/pages/ResetPasswordPage/ResetPasswordPage.tsx
+++ fin-track-app/src/pages/ResetPasswordPage/ResetPasswordPage.tsx
@@ -1,12 +1,33 @@
-import { Form, Input } from "antd";
+import { Button, Form, Input, message } from "antd";
+import { useState } from "react";
+import { resetPassword } from "../../shared/config/authReset";
 
 
 export default function ResetPasswordPage() {
+  const [loading, setLoading] = useState(false);
+
+  const handleResetPassword = async ({email}: {email: string}) => {
+    try {
+      setLoading(true);
+      await resetPassword(email);
+      message.success('Проверьте почту для восстановления пароля');
+    } catch (error: any) {
+      message.error('Ошибка при сбросе пароля')
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <Form>
-      <Form.Item>
-        <Input placeholder="Введите email"/>
+    <Form onFinish={handleResetPassword}>
+      <Form.Item
+      label='Email'
+      name='email'
+      rules={[{ required: true, message: "Введите email!" }]}
+      >
+        <Input type="email" placeholder="Введите email"/>
       </Form.Item>
+      <Button htmlType="submit" type="primary" loading={loading}>Восстановить пароль</Button>
     </Form>
   )
 }
