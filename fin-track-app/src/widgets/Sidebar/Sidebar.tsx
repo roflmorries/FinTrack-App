ded14ef4154407e44619eef856a5f57193343238
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-15 15:04:00 +0300

    update Sidebar widget

--- fin-track-app/src/widgets/Sidebar/Sidebar.tsx
+++ fin-track-app/src/widgets/Sidebar/Sidebar.tsx
@@ -1,11 +1,33 @@
-import { Avatar } from "antd"
-import { useAppSelector } from "../../shared/lib/hooks/redux/reduxTypes"
+import { Avatar, Button } from "antd"
+import { useAppDispatch, useAppSelector } from "../../shared/lib/hooks/redux/reduxTypes"
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { userLogOut } from "../../entities/user/model/userThunks";
+
+const StyledNav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  margin-top: 50px;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  .active {
+    color: purple;
+  }
+`
 
 type SidebarProps = {
   className?: string,
 }
 
 export default function Sidebar({ className }: SidebarProps) {
+  const dispatch = useAppDispatch();
+
+  const handleUserLogOut = () => {
+    dispatch(userLogOut());
+  }
 
   const user = useAppSelector(state => state.user.currentUser);
   return (
@@ -14,6 +36,20 @@ export default function Sidebar({ className }: SidebarProps) {
          <Avatar size={92} icon={<img src={user?.avatar}></img>} />
          <p>{user?.fullName}</p>
          <p>Balance must be here..</p>
+      </div>
+      <StyledNav className="sidebar__navigation">
+        <NavLink to='/dashboard' end>
+          Dashboard
+        </NavLink>
+        <NavLink to='/dashboard/transactions'>
+          Transactions
+        </NavLink>
+        <NavLink to='/dashboard/categories'>
+          Categories
+        </NavLink>
+      </StyledNav>
+      <div>
+        <Button onClick={handleUserLogOut}>LogOut</Button>
       </div>
     </div>
   )
