764c03e7b1a5e948c3476a7313cc08ebe7220ca9
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-15 15:04:20 +0300

    update app

--- fin-track-app/src/App/App.tsx
+++ fin-track-app/src/App/App.tsx
@@ -1,26 +1,29 @@
 import { BrowserRouter } from 'react-router-dom'
 import '../shared/ui/App.css'
 import AppRoutes from './providers/router/AppRoutes'
-import { useAppDispatch } from '../shared/lib/hooks/redux/reduxTypes'
+import { useAppDispatch, useAppSelector } from '../shared/lib/hooks/redux/reduxTypes'
 import { onAuthStateChanged } from 'firebase/auth';
 import { fetchUserData } from '../entities/user/model/userThunks';
 import { auth } from '../shared/config/firebase';
+import { useEffect } from 'react';
+import Loader from '../widgets/Loader/Loader';
 
 function App() {
 
   const dispatch = useAppDispatch();
+  const isLoading = useAppSelector(state => state.user.isLoading)
 
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
-  //     if (firebaseUser) {
-  //       dispatch(fetchUserData(firebaseUser.uid));
-  //     }
-  //   });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      if (firebaseUser) {
+        dispatch(fetchUserData(firebaseUser.uid));
+      }
+    });
 
-  //   return () => unsubscribe();
-  // }, [dispatch]);
+    return () => unsubscribe();
+  }, [dispatch]);
 
-
+  if(isLoading) return <Loader/>
   return (
     <BrowserRouter>
       <AppRoutes/>
