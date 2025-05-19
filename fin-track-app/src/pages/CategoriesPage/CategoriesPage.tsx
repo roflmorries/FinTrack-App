e72a5f4098387c4690fb792d68e44952ea3da4cd
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-20 00:32:45 +0300

    update categories form

--- fin-track-app/src/pages/CategoriesPage/CategoriesPage.tsx
+++ fin-track-app/src/pages/CategoriesPage/CategoriesPage.tsx
@@ -1,4 +1,11 @@
 import styled from "styled-components"
+import { useAppDispatch, useAppSelector } from "../../shared/lib/hooks/redux/reduxTypes";
+import { selectAllCategories } from "../../entities/categories/model/categorySelectors";
+import { useState } from "react";
+import CategoriesList from "../../features/categories/CategoriesList";
+import { Modal } from "antd";
+import CategoryForm from "../../features/categories/CategoryForm";
+import { deleteCategory } from "../../entities/categories/model/categorySlice";
 
 const Layout = styled.div`
   background-color: #141414;
@@ -9,9 +16,38 @@ const Layout = styled.div`
 `
 
 export default function CategoriesPage() {
+  const data = useAppSelector(selectAllCategories)
+  const dispatch = useAppDispatch();
+  // const [isAddFormShown, setIsAddFormShown] = useState(false);
+  const [isEditModalShown, setIsEditModalShown] = useState(false);
+  const [editCategoryId, setEditCategoryId] = useState<string | null>(null);
+
+  const handleTransactionEdit = (categoryId: string) => {
+    setIsEditModalShown(true)
+    setEditCategoryId(categoryId)
+  }
+
+  const handleTransactionDelete = (categoryId: string) => {
+    dispatch(deleteCategory(categoryId));
+  }
+
+  const handleCloseModal = () => {
+    setIsEditModalShown(false)
+  }
+
   return (
     <Layout>
-      <div>CategoriesPage</div>
+      <div>TransactionPage</div>
+      <CategoryForm onSave={handleCloseModal}/>
+      <CategoriesList items={data} onEdit={handleTransactionEdit} onDelete={handleTransactionDelete}/>
+      <Modal
+      title='edit form'
+      open={isEditModalShown}
+      onCancel={handleCloseModal}
+      destroyOnClose // mb delete
+      >
+        <CategoryForm categoryId={editCategoryId ?? undefined} onSave={handleCloseModal}/>
+      </Modal>
     </Layout>
   )
 }
