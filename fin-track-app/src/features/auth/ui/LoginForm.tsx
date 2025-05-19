9a6a3992f433aa5fa35f9e79064dfa8afc042856
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-19 18:13:41 +0300

    update Login for Transaction

--- fin-track-app/src/features/auth/ui/LoginForm.tsx
+++ fin-track-app/src/features/auth/ui/LoginForm.tsx
@@ -4,6 +4,40 @@ import { useAppDispatch, useAppSelector } from "../../../shared/lib/hooks/redux/
 import { loginUser } from "../../../entities/user/model/userThunks";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import styled from "styled-components";
+import { browserLocalPersistence, browserSessionPersistence, setPersistence } from "firebase/auth";
+import { auth } from "../../../shared/config/firebase";
+
+const CustomForm = styled.div`
+  width: 35%;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #a100ff;
+  min-height: 70vh;
+  align-items: center;
+  
+
+  .ant-form {
+    width: 90%;
+  }
+`
+
+const HeaderForm = styled.div`
+  img {
+    width: 100px;
+    height: 100px;
+  }
+  h3 {
+    font-size: 20px;
+    margin: 0;
+  }
+  p {
+    margin: 0;
+    font-size: 14px;
+    font-weight: 300;
+  }
+`
 
 type Props = {}
 
@@ -18,8 +52,11 @@ export default function LoginForm({}: Props) {
     }
   }, [isAuth, navigate]);
 
-  const handleLoginForm = async (values: {email: string; password: string}) => {
+  const handleLoginForm = async (values: {email: string; password: string; rememberMe: boolean}) => {
     try {
+      await setPersistence(
+        auth, values.rememberMe ? browserLocalPersistence : browserSessionPersistence
+      );
       await dispatch(loginUser(values))
     } catch(error) {
       console.error('Login error:', error)
@@ -27,11 +64,19 @@ export default function LoginForm({}: Props) {
   }
 
   return (
-    <>
+    <CustomForm>
+
+      <HeaderForm>
+        <img src="/icon.png"></img>
+        <h3>Welcome to FinTrack</h3>
+        <p>Log in using the form below</p>
+      </HeaderForm>
+
     {error && <div>Ошибка: {error}</div>}
     <Form
     name='Login'
     onFinish={handleLoginForm}
+    layout="vertical"
     >
 
       <Form.Item
@@ -54,10 +99,17 @@ export default function LoginForm({}: Props) {
       >
         <Input placeholder="Введите пароль"/>
       </Form.Item>
+
+      <Form.Item
+      name='rememberMe'
+      valuePropName="checked"
+      >
+        <Input type="checkbox"/>Remember Me
+      </Form.Item>
       <Button type="primary" htmlType="submit">Войти</Button>
     </Form>
     <GoogleLoginButton/>
     <Button onClick={() => navigate('/reset-password')}>Forget Password?</Button>
-    </>
+    </CustomForm>
   )
 }
