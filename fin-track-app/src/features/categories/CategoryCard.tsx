d46ba0ba8c8d51a2861a8504ee4add4987857258
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-20 15:52:07 +0300

    update Category Card

--- fin-track-app/src/features/categories/CategoryCard.tsx
+++ fin-track-app/src/features/categories/CategoryCard.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components';
 import { Category } from '../../entities/categories/model/types';
 import { Button } from 'antd';
 
@@ -6,14 +7,29 @@ type CategoryCardProps = Category & {
   onDelete: (id: string) => void;
 }
 
+const StyledCard = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 30px;
+  align-items: center;
+  p {
+    color: white;
+  }
+  .buttons__container {
+    display: flex;
+    gap: 15px;
+  }
+`
+
+
 export default function CategoryCard({ onEdit, onDelete, ...category }: CategoryCardProps) {
   return (
-    <>
+    <StyledCard>
     <p>Category name: {category.name}</p>
-    <div>
+    <div className='buttons__container'>
       <Button type='primary' onClick={() => onEdit(category.id)}>Edit</Button>
       <Button type='primary' onClick={() => onDelete(category.id)}>Delete</Button>
     </div>
-    </>
+    </StyledCard>
   )
 }
