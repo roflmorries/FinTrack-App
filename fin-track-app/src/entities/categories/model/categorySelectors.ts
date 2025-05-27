6abce0d922c6c20513bf20fbb642974f6389da10
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-27 15:30:55 +0300

    update Category Selector

--- fin-track-app/src/entities/categories/model/categorySelectors.ts
+++ fin-track-app/src/entities/categories/model/categorySelectors.ts
@@ -1,7 +1,8 @@
+import { RootState } from "../../../app/store/store";
 import { categoryAdapter } from "./categorySlice"
 
 export const {
   selectAll: selectAllCategories,
   selectById: selectCategoryById,
   selectIds: selectCategoryIds
-} = categoryAdapter.getSelectors((state: any) => state.category);
+} = categoryAdapter.getSelectors((state: RootState) => state.category);
