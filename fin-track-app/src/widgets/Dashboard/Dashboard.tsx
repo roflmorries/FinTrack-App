95c3f80ef8de72040df8030594bb07f5d62849fd
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-27 17:36:46 +0300

    testing progress bar

--- fin-track-app/src/widgets/Dashboard/Dashboard.tsx
+++ fin-track-app/src/widgets/Dashboard/Dashboard.tsx
@@ -1,4 +1,8 @@
 import styled from "styled-components"
+import { useAppSelector } from "../../shared/lib/hooks/redux/reduxTypes"
+import { selectGoalProgress } from "../../entities/fin-goals/goalProgressSelector"
+import { Progress } from "antd";
+import { selectAllGoals } from "../../entities/fin-goals/goalSelectors";
 
 const Layout = styled.div`
   background-color: #141414;
@@ -6,12 +10,29 @@ const Layout = styled.div`
   border-radius: 24px;
   padding: 1px;
   margin: 20px;
+  * {
+    color: white
+  }
 `
 
 export default function Dashboard() {
+  const goals = useAppSelector(selectAllGoals);
+
   return (
     <Layout>
       <p>DashboardLALALLALALA</p>
+      {goals.map(goal => {
+      const progress = useAppSelector(state => selectGoalProgress(state, goal.id));
+      const percent = Math.min((progress / goal.amount) * 100, 100);
+      return (
+        <div key={goal.id}>
+          <p>{goal.name}</p>
+          <Progress percent={percent} />
+          <p>{progress} / {goal.amount}</p>
+        </div>
+      );
+    })}
+      
     </Layout>
   )
 }
