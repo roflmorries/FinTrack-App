0435b9c19d80227d382296ea3c5d1a84258c0ba1
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-28 23:05:45 +0300

    improved balance selector & added goals selector

--- fin-track-app/src/entities/transactions/model/selectBalance.ts
+++ fin-track-app/src/entities/transactions/model/selectBalance.ts
@@ -1,12 +1,30 @@
-import { RootState } from "../../../app/store/store";
+import { createSelector } from "@reduxjs/toolkit";
 import { SelectAllTransactions } from "./transactionsSelectors";
 
 
-export const selectBalance = (state: RootState) => {
-  const transactions = SelectAllTransactions(state);
-  return transactions.reduce((acc, tx) => {
-    if (tx.type === 'income') return acc + tx.amount;
-    if (tx.type === 'expense') return acc - tx.amount;
-    return acc;
-  }, 0)
-}
+export const selectBalance = createSelector(
+  [SelectAllTransactions],
+  (transactions) => 
+    transactions.reduce((acc, tx) => {
+      if (tx.type === 'income') return acc + tx.amount;
+      if (tx.type === 'expense') return acc - tx.amount;
+      return acc
+    }, 0)
+)
+
+export const selectGoalsReserved = createSelector(
+  [SelectAllTransactions],
+  (transactions) =>
+    transactions
+      .filter(tx => !!tx.goalId)
+      .reduce((acc, tx) => {
+        if (tx.type === "income") return acc + tx.amount;
+        if (tx.type === "expense") return acc - tx.amount;
+        return acc;
+      }, 0)
+);
+
+export const selectFreeBalance = createSelector(
+  [selectBalance, selectGoalsReserved],
+  (total, reserved) => total - reserved
+)
