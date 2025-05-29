ebfcf23311da51c98ee0a4b42ef1cb5e1c22769c
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-29 23:47:26 +0300

    improved goals deleting logic

--- fin-track-app/src/features/goals/GoalCard.tsx
+++ fin-track-app/src/features/goals/GoalCard.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { Goal } from '../../entities/fin-goals/types'
-import { Button } from 'antd'
+import { Button, Popover } from 'antd'
+import { useState } from 'react'
 
 type GoalCardProps = Goal & {
   onEdit: (id: string) => void,
@@ -21,6 +22,15 @@ const StyledContainer = styled.div`
 `
 
 export default function GoalCard({onEdit, onDelete, ...goal}: GoalCardProps) {
+  const [open, setOpen] = useState(false);
+
+  const hide = () => {
+    setOpen(false);
+  };
+
+  const handleOpenChange = (newOpen: boolean) => {
+    setOpen(newOpen);
+  };
   return (
     <StyledContainer>
     <p>Name: {goal.name}</p>
@@ -28,7 +38,23 @@ export default function GoalCard({onEdit, onDelete, ...goal}: GoalCardProps) {
     <p>Deadline: {goal.deadline}</p>
     <div className='buttons__container'>
       <Button type='primary' onClick={() => onEdit(goal.id)}>Edit</Button>
-      <Button type='primary' onClick={() => onDelete(goal.id)}>Delete</Button>
+      {/* <Button type='primary' onClick={() => onDelete(goal.id)}>Delete</Button> */}
+      <Popover
+      content={
+        <>
+        <p>Удаление цели не повлияет на ваши транзакции и баланс.</p>
+        <Button type='primary' onClick={() => onDelete(goal.id)}>Delete</Button>
+        <Button onClick={hide}>Close</Button>
+        </>
+      }
+      title="Title"
+      trigger="click"
+      open={open}
+      onOpenChange={handleOpenChange}
+    >
+      <Button type="primary">Delete</Button>
+    </Popover>
+
     </div>
     </StyledContainer>
   )
