b36fe4ce467221a609b5a97a1b6c074e16afb840
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-05-31 22:54:55 +0300

    updated types & default categories w colors

--- fin-track-app/src/entities/categories/model/defaultCategories.ts
+++ fin-track-app/src/entities/categories/model/defaultCategories.ts
@@ -1,9 +1,9 @@
 import { Category } from "./types";
 
 export const defaultCategories: Omit<Category, "id" | "userId">[] = [
-  {name: 'Salary'},
-  {name: 'Food'},
-  {name: 'Transport'},
-  {name: 'Entertainment'},
-  {name: 'Goals'}
+  {name: 'Salary', color: '#4CAF50'},
+  {name: 'Food', color: '#FF9800'},
+  {name: 'Transport', color: '	#2196F3'},
+  {name: 'Entertainment', color: '	#9C27B0'},
+  {name: 'Goals', color: '	#FFC106'}
 ]
