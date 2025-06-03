08c029ded3b7f5e5c5a090b8385d7601b15de4a7
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-03 23:34:08 +0300

    added i18n

--- fin-track-app/src/main.tsx
+++ fin-track-app/src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './shared/config/i18n'
 import App from './app/App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store/store.ts'
