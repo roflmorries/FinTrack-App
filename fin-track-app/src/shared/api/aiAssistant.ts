a1a7481085c711debc4ab4bd01abc2eed2ee5eae
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-03 17:16:38 +0300

    clean up unused code

--- fin-track-app/src/shared/api/aiAssistant.ts
+++ fin-track-app/src/shared/api/aiAssistant.ts
@@ -1,23 +1,5 @@
-// import { OpenAI } from 'openai'
 import { Transaction } from '../../entities/transactions/model/types';
 
-// const openai = new OpenAI({ apiKey: import.meta.env.VITE_OPENAI_API_KEY, dangerouslyAllowBrowser: true });
-
-// export const askAssistant = async (question: string, transactions: Transaction[]) => {
-//   const prompt = `
-//     Мои транзакции за последний месяц:
-//     ${transactions.map(t => `${t.type === 'income' ? '+' : '-'}${t.amount} грн (${t.category})`).join('\n')}
-    
-//     Вопрос: ${question}
-//   `
-
-//   const response = await openai.chat.completions.create({
-//     model: 'gpt-3.5-turbo',
-//     messages: [{role: 'user', content: prompt}]
-//   });
-
-//   return response.choices[0]?.message.content;
-// }
 
 export const askAssistant = async (
   question: string,
