58c702895f857e1c3fcfe84bcc42f2144eae9ff6
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-06 15:52:32 +0300

    added routing for transactions

--- fin-track-backend/src/routes/transactions.ts
+++ fin-track-backend/src/routes/transactions.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
+import * as transactionController from '../controllers/transactionController'
 
 const router = Router();
 
+router.get('/', transactionController.getAll);
+router.post('/', transactionController.create);
+router.put('/:id', transactionController.update);
+router.delete('/:id', transactionController.remove);
 
 export default router;
