d85987e6a5ab6af044ee6b36841f18ce9ad4a646
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-11 15:48:12 +0300

    replaced localStorage mocks with real backend integration (categories)

--- fin-track-app/src/entities/categories/model/categorySlice.ts
+++ fin-track-app/src/entities/categories/model/categorySlice.ts
@@ -1,6 +1,6 @@
 import { createEntityAdapter, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Category } from "./types";
-import { fetchCategories, saveCategoriesToStorageMock } from "./categoryThunk";
+import { createCategory, deleteCategory, fetchCategories, updateCategory } from "./categoryThunk";
 
 
 export const categoryAdapter = createEntityAdapter<Category>();
@@ -14,34 +14,6 @@ const categorySlice = createSlice({
   name: 'categories',
   initialState,
   reducers: {
-    addCategory: (state, action: PayloadAction<Category>) => {
-      categoryAdapter.addOne(state, action.payload);
-      saveCategoriesToStorageMock({
-        userId: action.payload.userId,
-        categories: Object.values(state.entities)
-      });
-    },
-    updateCategory: (state, action: PayloadAction<{ id: string; changes: Partial<Category> }>) => {
-      categoryAdapter.updateOne(state, action.payload);
-      const updated = state.entities[action.payload.id];
-      if (updated) {
-        saveCategoriesToStorageMock({
-          userId: updated.userId,
-          categories: Object.values(state.entities),
-        })
-      }
-    },
-    deleteCategory: (state, action: PayloadAction<string>) => {
-      const id = action.payload;
-      const userId = state.entities[id]?.userId;
-      categoryAdapter.removeOne(state, id);
-      if (userId) {
-        saveCategoriesToStorageMock({
-          userId,
-          categories: Object.values(state.entities)
-        })
-      }
-    },
     setAllCategories: (state, action: PayloadAction<Category[]>) => {
       categoryAdapter.setAll(state, action.payload);
     }
@@ -60,9 +32,51 @@ const categorySlice = createSlice({
         state.loading = false;
         state.error = 'failed to fetch categories'
       })
+
+      .addCase(createCategory.fulfilled, (state, action) => {
+        state.loading = false;
+        categoryAdapter.addOne(state, action.payload);
+      })
+      .addCase(createCategory.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createCategory.rejected, state => {
+        state.loading = false;
+        state.error = 'Failed to create category'
+      })
+
+      .addCase(updateCategory.fulfilled, (state, action) => {
+        state.loading = false;
+        categoryAdapter.updateOne(state, {
+          id: action.payload.id,
+          changes: action.payload
+        })
+      })
+      .addCase(updateCategory.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateCategory.rejected, state => {
+        state.loading = false;
+        state.error = 'Failed to update category'
+      })
+
+      .addCase(deleteCategory.fulfilled, (state, action) => {
+        state.loading = false;
+        categoryAdapter.removeOne(state, action.payload)
+      })
+      .addCase(deleteCategory.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteCategory.rejected, state => {
+        state.loading = false;
+        state.error = 'Failed to delete category'
+      })
   }
 })
 
-export const {addCategory, updateCategory, deleteCategory, setAllCategories} = categorySlice.actions;
+// export const {setAllCategories} = categorySlice.actions;
 
 export default categorySlice.reducer;
--- fin-track-app/src/entities/categories/model/categoryThunk.ts
+++ fin-track-app/src/entities/categories/model/categoryThunk.ts
@@ -1,33 +1,56 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { Category } from "./types";
-import { defaultCategories } from "./defaultCategories";
-import { v4 as uuidv4 } from 'uuid';
+import { API_URL } from "../../../shared/config/config";
+import axios from "axios";
 
 
-// export const saveCategoriesToStorage = createAsyncThunk('categories/saveToStorage',
-//   async ({userId, categories}: {userId: string, categories: Category[]}) => {
-//     localStorage.setItem(`categories_${userId}`, JSON.stringify(categories));
-//     return categories;
+
+
+// export const fetchCategories = createAsyncThunk('categories/FetchCategories',
+//   async (userId: string) => {
+//     const data = localStorage.getItem(`categories_${userId}`);
+//     if (data) {
+//       return JSON.parse(data) as Category[];
+//     } else {
+//       const categories: Category[] = defaultCategories.map(category => ({
+//         ...category,
+//         id: uuidv4(),
+//         userId,
+//       }));
+//       localStorage.setItem(`categories${userId}`, JSON.stringify(categories));
+//       return categories;
+//     }
 //   }
 // );
 
-export const fetchCategories = createAsyncThunk('categories/FetchCategories',
-  async (userId: string) => {
-    const data = localStorage.getItem(`categories_${userId}`);
-    if (data) {
-      return JSON.parse(data) as Category[];
-    } else {
-      const categories: Category[] = defaultCategories.map(category => ({
-        ...category,
-        id: uuidv4(),
-        userId,
-      }));
-      localStorage.setItem(`categories${userId}`, JSON.stringify(categories));
-      return categories;
-    }
+// export const saveCategoriesToStorageMock = ({userId, categories} : { userId: string, categories: Category[] }) => {
+//   localStorage.setItem(`categories_${userId}`, JSON.stringify(categories));
+// }
+
+export const fetchCategories = createAsyncThunk<Category[], string>('categories/fetchAll',
+  async (userId) => {
+    const response = await axios.get<Category[]>(`${API_URL}?userId=${userId}`);
+    return response.data;
   }
 );
 
-export const saveCategoriesToStorageMock = ({userId, categories} : { userId: string, categories: Category[] }) => {
-  localStorage.setItem(`categories_${userId}`, JSON.stringify(categories));
-}
+export const createCategory = createAsyncThunk<Category, Omit<Category, 'id'>>('categories/create',
+  async (category) => {
+    const response = await axios.post<Category>(API_URL, category);
+    return response.data;
+  }
+);
+
+export const updateCategory = createAsyncThunk<Category, {id: string, changes: Partial<Category>}>('categories/update',
+  async ({id, changes}) => {
+    const response = await axios.put<Category>(`${API_URL}/${id}`, changes);
+    return response.data;
+  }
+);
+
+export const deleteCategory = createAsyncThunk<string, string>('categories/delete',
+  async id => {
+    await axios.delete(`${API_URL}/${id}`);
+    return id;
+  }
+);
