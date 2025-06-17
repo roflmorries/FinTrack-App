5fbcbabe0205e806c69a05550b86e3d1dab11c01
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-06-17 15:46:49 +0300

    replaced localStorage mocks with real backend integration (goals)

--- fin-track-app/src/entities/fin-goals/goalSlice.ts
+++ fin-track-app/src/entities/fin-goals/goalSlice.ts
@@ -1,6 +1,6 @@
 import { createEntityAdapter, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Goal } from "./types";
-import { fetchGoals, saveGoalToStorageMock } from "./goalThunk";
+import { createGoal, deleteGoal, fetchGoals, updateGoal} from "./goalThunk";
 
 
 export const goalAdapter = createEntityAdapter<Goal>();
@@ -14,34 +14,34 @@ const goalSlice = createSlice({
   name: 'goal',
   initialState: initialState,
   reducers: {
-    addGoal: (state, action: PayloadAction<Goal>) => {
-      goalAdapter.addOne(state, action.payload);
-      saveGoalToStorageMock({
-        userId: action.payload.userId,
-        goals: Object.values(state.entities)
-      });
-    },
-    updateGoal: (state, action: PayloadAction<{ id: string; changes: Partial<Goal> }>) => {
-      goalAdapter.updateOne(state, action.payload);
-      const updated = state.entities[action.payload.id];
-      if (updated) {
-        saveGoalToStorageMock({
-          userId: updated.userId,
-          goals: Object.values(state.entities)
-        })
-      }
-    },
-    deleteGoal: (state, action: PayloadAction<string>) => {
-      const id = action.payload;
-      const userId = state.entities[id]?.userId;
-      goalAdapter.removeOne(state, action.payload);
-      if (userId) {
-        saveGoalToStorageMock({
-          userId,
-          goals: Object.values(state.entities)
-        })
-      }
-    }
+    // addGoal: (state, action: PayloadAction<Goal>) => {
+    //   goalAdapter.addOne(state, action.payload);
+    //   saveGoalToStorageMock({
+    //     userId: action.payload.userId,
+    //     goals: Object.values(state.entities)
+    //   });
+    // },
+    // updateGoal: (state, action: PayloadAction<{ id: string; changes: Partial<Goal> }>) => {
+    //   goalAdapter.updateOne(state, action.payload);
+    //   const updated = state.entities[action.payload.id];
+    //   if (updated) {
+    //     saveGoalToStorageMock({
+    //       userId: updated.userId,
+    //       goals: Object.values(state.entities)
+    //     })
+    //   }
+    // },
+    // deleteGoal: (state, action: PayloadAction<string>) => {
+    //   const id = action.payload;
+    //   const userId = state.entities[id]?.userId;
+    //   goalAdapter.removeOne(state, action.payload);
+    //   if (userId) {
+    //     saveGoalToStorageMock({
+    //       userId,
+    //       goals: Object.values(state.entities)
+    //     })
+    //   }
+    // }
   },
   extraReducers: builder => {
     builder
@@ -57,8 +57,50 @@ const goalSlice = createSlice({
         state.loading = false;
         state.error = 'Failed to fetch goals'
       })
+
+      .addCase(createGoal.fulfilled, (state, action) => {
+        state.loading = false;
+        goalAdapter.addOne(state, action.payload)
+      })
+      .addCase(createGoal.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createGoal.rejected, state => {
+        state.loading = false;
+        state.error = 'Failed to create goal'
+      })
+
+      .addCase(updateGoal.fulfilled, (state, action) => {
+        state.loading = false;
+        goalAdapter.updateOne(state, {
+          id: action.payload.id,
+          changes: action.payload
+        })
+      })
+      .addCase(updateGoal.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateGoal.rejected, state => {
+        state.loading = false;
+        state.error = 'Failed to update goal'
+      })
+
+      .addCase(deleteGoal.fulfilled, (state, action) => {
+        state.loading = false;
+        goalAdapter.removeOne(state, action.payload)
+      })
+      .addCase(deleteGoal.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteGoal.rejected, state => {
+        state.loading = false;
+        state.error = 'Failed to delete goal'
+      })
   }
 })
 
-export const { addGoal, updateGoal, deleteGoal } = goalSlice.actions;
+// export const { addGoal, updateGoal, deleteGoal } = goalSlice.actions;
 export default goalSlice.reducer;
--- fin-track-app/src/entities/fin-goals/goalThunk.ts
+++ fin-track-app/src/entities/fin-goals/goalThunk.ts
@@ -1,15 +1,34 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { Goal } from "./types";
+import axios from "axios";
+import { API_URL } from "../../shared/config/config";
 
+const API_URL_GOALS = `${API_URL}/goals`
 
 export const fetchGoals = createAsyncThunk<Goal[], string>('goals/fetchAll',
   async (userId: string) => {
-    const data = localStorage.getItem(`goals_${userId}`);
-    return data ? JSON.parse(data) : []
+    const response = await axios.get<Goal[]>(`${API_URL_GOALS}?userId=${userId}`);
+    return response.data
     }
 );
 
+export const createGoal = createAsyncThunk<Goal, Omit<Goal, 'id'>>('goals/create',
+  async (goal) => {
+    const response = await axios.post<Goal>(API_URL_GOALS, goal);
+    return response.data;
+  }
+);
 
-export const saveGoalToStorageMock = ({userId, goals}: {userId: string, goals: Goal[]}) => {
-  localStorage.setItem(`goals_${userId}`, JSON.stringify(goals))
-}
+export const updateGoal = createAsyncThunk<Goal, {id: string, changes: Partial<Goal>}>('goals/update',
+  async ({ id, changes }) => {
+    const response = await axios.put<Goal>(`${API_URL_GOALS}/${id}`, changes);
+    return response.data;
+  }
+);
+
+export const deleteGoal = createAsyncThunk<string, string>('goals/delete',
+  async id => {
+    await axios.delete(`${API_URL_GOALS}/${id}`);
+    return id;
+  }
+);
