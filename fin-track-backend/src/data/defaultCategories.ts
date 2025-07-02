a5cbcef08ab2fbb175339b62beb077a840b624d5
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-07-02 15:00:53 +0300

    add default categories

---
+++ fin-track-backend/src/data/defaultCategories.ts
@@ -0,0 +1,7 @@
+export const defaultCategories = [
+  { name: 'Salary', color: '#4CAF50' },
+  { name: 'Food', color: '#FF9800' },
+  { name: 'Transport', color: '#2196F3' },
+  { name: 'Entertainment', color: '#9C27B0' },
+  { name: 'Goals', color: '#FFC106' }
+];
