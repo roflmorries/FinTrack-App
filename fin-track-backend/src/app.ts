93b83c41513be3149ee6428652c8e0c26af8daa8
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-07-04 16:26:04 +0300

    add more limit for json

--- fin-track-backend/src/app.ts
+++ fin-track-backend/src/app.ts
@@ -4,15 +4,17 @@ import transactionsRouter from './routes/transactions';
 import categoriesRouter from './routes/categories';
 import detectCategoryRouter from './routes/detectCategory'
 import goalsRouter from './routes/goals'
+import userRouter from './routes/users'
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '2mb' }));
 
 app.use('/transactions', transactionsRouter);
 app.use('/categories', categoriesRouter);
 app.use('/detect-category', detectCategoryRouter);
 app.use('/goals', goalsRouter);
+app.use('/users', userRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server runs on port ${PORT}`))
