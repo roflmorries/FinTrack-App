a49e8f54c626f342b2747ee255e47197d38222d2
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-07-05 21:20:49 +0300

    add axios instance w/ auto added token

---
+++ fin-track-app/src/shared/api/axiosWithAuth.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { getIdToken } from "firebase/auth";
+import { auth } from "../config/firebase";
+import { API_URL } from "../config/config";
+
+export const api = axios.create({baseURL: API_URL});
+
+api.interceptors.request.use(async (config:any) => {
+  const user = auth.currentUser;
+  if (user) {
+    const token = await getIdToken(user);
+    config.headers = {
+      ...config.headers,
+      Authorization: `Bearer ${token}`
+    };
+  }
+  return config
+})
