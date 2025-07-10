36462598c59a9d5c897c36a27cb0a956e1ec6cdb
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-07-10 22:36:44 +0300

    update auth forms w/ using MUI

--- fin-track-app/src/features/auth/ui/LoginForm.tsx
+++ fin-track-app/src/features/auth/ui/LoginForm.tsx
@@ -1,50 +1,200 @@
-import { Button, Form, Input } from "antd"
+// import { Button, Form, Input } from "antd"
+// import GoogleLoginButton from "./GoogleLoginButton"
+// import { useAppDispatch, useAppSelector } from "../../../shared/lib/hooks/redux/reduxTypes"
+// import { loginUser } from "../../../entities/user/model/userThunks";
+// import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import styled from "styled-components";
+// import { browserLocalPersistence, browserSessionPersistence, setPersistence } from "firebase/auth";
+// import { auth } from "../../../shared/config/firebase";
+
+// const CustomForm = styled.div`
+//   width: 35%;
+//   margin: 0 auto;
+//   display: flex;
+//   flex-direction: column;
+//   border: 1px solid #a100ff;
+//   min-height: 70vh;
+//   align-items: center;
+  
+
+//   .ant-form {
+//     width: 90%;
+//   }
+// `
+
+// const HeaderForm = styled.div`
+//   img {
+//     width: 100px;
+//     height: 100px;
+//   }
+//   h3 {
+//     font-size: 20px;
+//     margin: 0;
+//   }
+//   p {
+//     margin: 0;
+//     font-size: 14px;
+//     font-weight: 300;
+//   }
+// `
+
+// type Props = {}
+
+// export default function LoginForm({}: Props) {
+//   const dispatch = useAppDispatch();
+//   const navigate = useNavigate();
+//   const { isAuth, error} = useAppSelector((state) => state.user)
+
+//   useEffect(() => {
+//     if (isAuth) {
+//       navigate('/dashboard')
+//     }
+//   }, [isAuth, navigate]);
+
+//   const handleLoginForm = async (values: {email: string; password: string; rememberMe: boolean}) => {
+//     try {
+//       await setPersistence(
+//         auth, values.rememberMe ? browserLocalPersistence : browserSessionPersistence
+//       );
+//       await dispatch(loginUser(values))
+//     } catch(error) {
+//       console.error('Login error:', error)
+//     }
+//   }
+
+//   return (
+//     <CustomForm>
+
+//       <HeaderForm>
+//         <img src="/icon.png"></img>
+//         <h3>Welcome to FinTrack</h3>
+//         <p>Log in using the form below</p>
+//       </HeaderForm>
+
+//     {error && <div>Ошибка: {error}</div>}
+//     <Form
+//     name='Login'
+//     onFinish={handleLoginForm}
+//     layout="vertical"
+//     >
+
+//       <Form.Item
+//       label='Email'
+//       name='email'
+//       rules={[
+//         { required: true, message: 'Введите email' },
+//         { type: 'email', message: 'Некорректный email' },
+//       ]}
+//       >
+//         <Input placeholder="Введите Email" />
+//       </Form.Item>
+
+//       <Form.Item
+//       label='Password'
+//       name='password'
+//       rules={[
+//         { required: true, message: 'Введите email' },
+//       ]}
+//       >
+//         <Input placeholder="Введите пароль"/>
+//       </Form.Item>
+
+//       <Form.Item
+//       name='rememberMe'
+//       valuePropName="checked"
+//       >
+//         <Input type="checkbox"/>Remember Me
+//       </Form.Item>
+//       <Button type="primary" htmlType="submit">Войти</Button>
+//     </Form>
+//     <GoogleLoginButton/>
+//     <Button onClick={() => navigate('/reset-password')}>Forget Password?</Button>
+//     </CustomForm>
+//   )
+// }
+
+
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Divider from '@mui/material/Divider';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import MuiCard from '@mui/material/Card';
+import { styled } from '@mui/material/styles';
+import AppTheme from '../../../shared/shared-theme/AppTheme';
+import ColorModeSelect from '../../../shared/shared-theme/ColorModeSelect';
+import { FacebookIcon, SitemarkIcon } from '../../../pages/LoginPage/components/CustomIcons';
+import { useState } from 'react';
 import GoogleLoginButton from "./GoogleLoginButton"
 import { useAppDispatch, useAppSelector } from "../../../shared/lib/hooks/redux/reduxTypes"
 import { loginUser } from "../../../entities/user/model/userThunks";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import styled from "styled-components";
 import { browserLocalPersistence, browserSessionPersistence, setPersistence } from "firebase/auth";
 import { auth } from "../../../shared/config/firebase";
 
-const CustomForm = styled.div`
-  width: 35%;
-  margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  border: 1px solid #a100ff;
-  min-height: 70vh;
-  align-items: center;
-  
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  borderRadius: `calc(${theme.shape.borderRadius}px + 26px)`,
+  width: '100%',
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: 'auto',
+  [theme.breakpoints.up('sm')]: {
+    maxWidth: '450px',
+  },
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  ...theme.applyStyles('dark', {
+    boxShadow:
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
+}));
 
-  .ant-form {
-    width: 90%;
-  }
-`
+const SignInContainer = styled(Stack)(({ theme }) => ({
+  height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+  minHeight: '100%',
+  padding: theme.spacing(2),
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(4),
+  },
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage:
+        'radial-gradient(at 50% 50%, hsla(270, 100%, 22%, 0.5), hsl(270, 30%, 5%))',
+    }),
+  },
+}));
 
-const HeaderForm = styled.div`
-  img {
-    width: 100px;
-    height: 100px;
-  }
-  h3 {
-    font-size: 20px;
-    margin: 0;
-  }
-  p {
-    margin: 0;
-    font-size: 14px;
-    font-weight: 300;
-  }
-`
-
-type Props = {}
-
-export default function LoginForm({}: Props) {
+export default function LoginForm(props: { disableCustomTheme?: boolean }) {
+  const [emailError, setEmailError] = useState(false);
+  const [emailErrorMessage, setEmailErrorMessage] = useState('');
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  const [open, setOpen] = useState(false);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { isAuth, error} = useAppSelector((state) => state.user)
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (isAuth) {
@@ -52,64 +202,169 @@ export default function LoginForm({}: Props) {
     }
   }, [isAuth, navigate]);
 
-  const handleLoginForm = async (values: {email: string; password: string; rememberMe: boolean}) => {
+  const handleLoginForm = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email') as string;
+    const password = data.get('password') as string;
+    const rememberMe = Boolean(data.get('rememberMe'));
+
+    setLoading(true);
     try {
       await setPersistence(
-        auth, values.rememberMe ? browserLocalPersistence : browserSessionPersistence
+        auth, rememberMe ? browserLocalPersistence : browserSessionPersistence
       );
-      await dispatch(loginUser(values))
+      await dispatch(loginUser({ email, password}));
     } catch(error) {
       console.error('Login error:', error)
+    } finally {
+      setLoading(false);
     }
   }
 
+
+
+  // const validateInputs = () => {
+  //   const email = document.getElementById('email') as HTMLInputElement;
+  //   const password = document.getElementById('password') as HTMLInputElement;
+
+  //   let isValid = true;
+
+  //   if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+  //     setEmailError(true);
+  //     setEmailErrorMessage('Please enter a valid email address.');
+  //     isValid = false;
+  //   } else {
+  //     setEmailError(false);
+  //     setEmailErrorMessage('');
+  //   }
+
+  //   if (!password.value || password.value.length < 6) {
+  //     setPasswordError(true);
+  //     setPasswordErrorMessage('Password must be at least 6 characters long.');
+  //     isValid = false;
+  //   } else {
+  //     setPasswordError(false);
+  //     setPasswordErrorMessage('');
+  //   }
+
+  //   return isValid;
+  // };
+
   return (
-    <CustomForm>
-
-      <HeaderForm>
-        <img src="/icon.png"></img>
-        <h3>Welcome to FinTrack</h3>
-        <p>Log in using the form below</p>
-      </HeaderForm>
-
-    {error && <div>Ошибка: {error}</div>}
-    <Form
-    name='Login'
-    onFinish={handleLoginForm}
-    layout="vertical"
-    >
-
-      <Form.Item
-      label='Email'
-      name='email'
-      rules={[
-        { required: true, message: 'Введите email' },
-        { type: 'email', message: 'Некорректный email' },
-      ]}
-      >
-        <Input placeholder="Введите Email" />
-      </Form.Item>
-
-      <Form.Item
-      label='Password'
-      name='password'
-      rules={[
-        { required: true, message: 'Введите email' },
-      ]}
-      >
-        <Input placeholder="Введите пароль"/>
-      </Form.Item>
-
-      <Form.Item
-      name='rememberMe'
-      valuePropName="checked"
-      >
-        <Input type="checkbox"/>Remember Me
-      </Form.Item>
-      <Button type="primary" htmlType="submit">Войти</Button>
-    </Form>
-    <GoogleLoginButton/>
-    <Button onClick={() => navigate('/reset-password')}>Forget Password?</Button>
-    </CustomForm>
-  )
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+      <SignInContainer direction="column" justifyContent="space-between">
+        <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
+        <Card variant="outlined">
+          <SitemarkIcon />
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)'}}
+          >
+            Sign in
+          </Typography>
+          {error && <Typography color="error">Ошибка: {error}</Typography>}
+          <Box
+            component="form"
+            onSubmit={handleLoginForm}
+            noValidate
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              width: '100%',
+              gap: 2,
+            }}
+          >
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <TextField
+                id="email"
+                type="email"
+                name="email"
+                placeholder="your@email.com"
+                autoComplete="email"
+                // autoFocus
+                required
+                fullWidth
+                variant="outlined"
+                color={emailError ? 'error' : 'primary'}
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    borderRadius: '16px', // твой радиус
+                  }
+                }}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <TextField
+                name="password"
+                placeholder="••••••"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                required
+                fullWidth
+                variant="outlined"
+                color={error ? 'error' : 'primary'}
+                sx={{
+                  '& .MuiOutlinedInput-root': {
+                    borderRadius: '16px', // твой радиус
+                  }
+                }}
+              />
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox value="rememberMe" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              disabled={loading}
+              sx={{ borderRadius: '16px' }}
+            >
+              Sign in
+            </Button>
+            <Link
+              component="button"
+              type="button"
+              onClick={() => navigate('/reset-password')}
+              variant="body2"
+              sx={{ alignSelf: 'center' }}
+            >
+              Forgot your password?
+            </Link>
+          </Box>
+          <Divider>or</Divider>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <GoogleLoginButton/>
+            <Button
+              fullWidth
+              variant="outlined"
+              onClick={() => alert('Sign in with Facebook')}
+              startIcon={<FacebookIcon />}
+              sx={{ borderRadius: '16px' }}
+            >
+              Sign in with Facebook
+            </Button>
+            <Typography sx={{ textAlign: 'center' }}>
+              Don&apos;t have an account?{' '}
+              <Link
+                component='button'
+                variant="body2"
+                sx={{ alignSelf: 'center' }}
+                onClick={() => navigate('/registration')}
+              >
+                Sign up
+              </Link>
+            </Typography>
+          </Box>
+        </Card>
+      </SignInContainer>
+    </AppTheme>
+  );
 }
--- fin-track-app/src/features/auth/ui/RegistrationForm.tsx
+++ fin-track-app/src/features/auth/ui/RegistrationForm.tsx
@@ -1,14 +1,170 @@
-import { Button, Form, Input, Upload } from 'antd'
-import { RcFile } from 'antd/es/upload';
+// import { Button, Form, Input, Upload } from 'antd'
+// import { RcFile } from 'antd/es/upload';
+// import { useEffect, useState } from 'react'
+// import { registerUser } from '../../../entities/user/model/userThunks';
+// import { useAppDispatch, useAppSelector } from '../../../shared/lib/hooks/redux/reduxTypes';
+// import { useNavigate } from 'react-router-dom';
+// import { uploadAvatar } from '../../../shared/api/uploadAvatar';
+
+// type Props = {}
+
+// export default function RegistrationForm({}: Props) {
+//   const [avatarFile, setAvatarFile] = useState<File | null>(null);
+//   const [avatar, setAvatar] = useState<string | null>(null);
+//   const dispatch = useAppDispatch();
+//   const { isAuth } = useAppSelector((state) => state.user)
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     if (isAuth) {
+//       navigate('/dashboard', {replace: true})
+//     }
+//   }, [isAuth, navigate])
+
+
+
+//   const handleRegisterForm = async (values: any) => {
+//     try {
+//       let avatar = null;
+//       if (avatarFile) {
+//         avatar = await uploadAvatar(avatarFile)
+//       };
+//       const newUser = {...values, avatar}
+//       console.log(newUser)
+//       await dispatch(registerUser(newUser))} catch (error) {
+//         console.error(error);
+//       }
+//   }
+
+//   const handleUploadAvatar = (file: RcFile) => {
+//     setAvatarFile(file);
+//     setAvatar(URL.createObjectURL(file));
+//     return false;
+//   };
+
+//   return (
+//     <Form name='registration' onFinish={handleRegisterForm} autoComplete='off'>
+
+//       <Form.Item
+//       label='Fullname'
+//       name='fullName'
+//       rules={[{ required: true, message: 'Введите ваше имя!' }]}
+//       >
+//         <Input/>
+//       </Form.Item>
+
+//       <Form.Item
+//       label='Email'
+//       name='email'
+//       rules={[
+//         { required: true, message: 'Введите ваш email!' },
+//         { type: 'email', message: 'Некорректный email!' },
+//       ]}
+//       >
+//         <Input/>
+//       </Form.Item>
+
+//       <Form.Item
+//       label='Password'
+//       name='password'
+//       rules={[{ required: true, message: 'Введите ваш пароль!' }]}
+//       >
+//         <Input/>
+//       </Form.Item>
+
+//       <Form.Item
+//       label='Avatar'
+//       >
+//         <Upload
+//         beforeUpload={handleUploadAvatar}
+//         showUploadList={false}
+//         accept='image/*'
+//         >
+//           <Button>Upload Avatar</Button>
+//         </Upload>
+//         {avatar && (
+//           <img
+//           src={avatar}
+//           alt="avatar"
+//           style={{ marginTop: 10, maxWidth: 100, borderRadius: 8 }}
+//           />
+//         )}
+//       </Form.Item>
+//       <Button type="primary" htmlType="submit">Register</Button>
+//     </Form>
+//   )
+// }
+
+
+
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import MuiCard from '@mui/material/Card';
+import { styled } from '@mui/material/styles';
+import AppTheme from '../../../shared/shared-theme/AppTheme';
+import ColorModeSelect from '../../../shared/shared-theme/ColorModeSelect';
+import { GoogleIcon, FacebookIcon, SitemarkIcon } from '../../../pages/RegistrationPage/components/CustomIcons';
 import { useEffect, useState } from 'react'
 import { registerUser } from '../../../entities/user/model/userThunks';
 import { useAppDispatch, useAppSelector } from '../../../shared/lib/hooks/redux/reduxTypes';
 import { useNavigate } from 'react-router-dom';
 import { uploadAvatar } from '../../../shared/api/uploadAvatar';
+import GoogleLoginButton from './GoogleLoginButton';
 
-type Props = {}
+const Card = styled(MuiCard)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignSelf: 'center',
+  width: '100%',
+  padding: theme.spacing(4),
+  gap: theme.spacing(2),
+  margin: 'auto',
+  boxShadow:
+    'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+  [theme.breakpoints.up('sm')]: {
+    width: '450px',
+  },
+  ...theme.applyStyles('dark', {
+    boxShadow:
+      'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+  }),
+}));
 
-export default function RegistrationForm({}: Props) {
+const SignUpContainer = styled(Stack)(({ theme }) => ({
+  height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+  minHeight: '100%',
+  padding: theme.spacing(2),
+  [theme.breakpoints.up('sm')]: {
+    padding: theme.spacing(4),
+  },
+  '&::before': {
+    content: '""',
+    display: 'block',
+    position: 'absolute',
+    zIndex: -1,
+    inset: 0,
+    backgroundImage:
+      'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+    backgroundRepeat: 'no-repeat',
+    ...theme.applyStyles('dark', {
+      backgroundImage:
+        'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+    }),
+  },
+}));
+
+export default function RegistrationForm(props: { disableCustomTheme?: boolean }) {
   const [avatarFile, setAvatarFile] = useState<File | null>(null);
   const [avatar, setAvatar] = useState<string | null>(null);
   const dispatch = useAppDispatch();
@@ -21,76 +177,150 @@ export default function RegistrationForm({}: Props) {
     }
   }, [isAuth, navigate])
 
+  const handleUploadAvatar = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0] || null;
+    setAvatarFile(file);
+    setAvatar(file ? URL.createObjectURL(file) : null);
+  };
 
-
-  const handleRegisterForm = async (values: any) => {
+  const handleRegisterForm = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const fullName = data.get('fullName') as string;
+    const email = data.get('email') as string;
+    const password = data.get('password') as string;
     try {
-      let avatar = null;
+      let avatar: string = '';
       if (avatarFile) {
         avatar = await uploadAvatar(avatarFile)
-      };
-      const newUser = {...values, avatar}
-      console.log(newUser)
-      await dispatch(registerUser(newUser))} catch (error) {
+      }
+      const newUser = { fullName, email, password, avatar };
+      console.log(newUser);
+      await dispatch(registerUser(newUser));
+    } catch (error) {
         console.error(error);
       }
   }
 
-  const handleUploadAvatar = (file: RcFile) => {
-    setAvatarFile(file);
-    setAvatar(URL.createObjectURL(file));
-    return false;
-  };
-
   return (
-    <Form name='registration' onFinish={handleRegisterForm} autoComplete='off'>
-
-      <Form.Item
-      label='Fullname'
-      name='fullName'
-      rules={[{ required: true, message: 'Введите ваше имя!' }]}
-      >
-        <Input/>
-      </Form.Item>
-
-      <Form.Item
-      label='Email'
-      name='email'
-      rules={[
-        { required: true, message: 'Введите ваш email!' },
-        { type: 'email', message: 'Некорректный email!' },
-      ]}
-      >
-        <Input/>
-      </Form.Item>
-
-      <Form.Item
-      label='Password'
-      name='password'
-      rules={[{ required: true, message: 'Введите ваш пароль!' }]}
-      >
-        <Input/>
-      </Form.Item>
-
-      <Form.Item
-      label='Avatar'
-      >
-        <Upload
-        beforeUpload={handleUploadAvatar}
-        showUploadList={false}
-        accept='image/*'
-        >
-          <Button>Upload Avatar</Button>
-        </Upload>
-        {avatar && (
-          <img
-          src={avatar}
-          alt="avatar"
-          style={{ marginTop: 10, maxWidth: 100, borderRadius: 8 }}
-          />
-        )}
-      </Form.Item>
-      <Button type="primary" htmlType="submit">Register</Button>
-    </Form>
-  )
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+      <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
+      <SignUpContainer direction="column" justifyContent="space-between">
+        <Card variant="outlined">
+          <SitemarkIcon />
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+          >
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleRegisterForm}
+            sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}
+          >
+            <FormControl>
+              <FormLabel htmlFor="fullName">Full name</FormLabel>
+              <TextField
+                autoComplete="fullName"
+                name="fullName"
+                required
+                fullWidth
+                id="fullName"
+                placeholder="Jon Snow"
+                // error={nameError}
+                // helperText={nameErrorMessage}
+                // color={nameError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="email">Email</FormLabel>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                placeholder="your@email.com"
+                name="email"
+                autoComplete="email"
+                variant="outlined"
+                // error={emailError}
+                // helperText={emailErrorMessage}
+                // color={passwordError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="password">Password</FormLabel>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                placeholder="••••••"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+                variant="outlined"
+                // error={passwordError}
+                // helperText={passwordErrorMessage}
+                // color={passwordError ? 'error' : 'primary'}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel htmlFor="avatar">Avatar</FormLabel>
+              <Button variant="outlined" component="label">
+                Upload Avatar
+                <input
+                  type="file"
+                  accept="image/*"
+                  hidden
+                  onChange={handleUploadAvatar}
+                  name="avatar"
+                />
+              </Button>
+              {avatar && (
+                <img
+                  src={avatar}
+                  alt="avatar"
+                  style={{ marginTop: 10, maxWidth: 100, borderRadius: 8 }}
+                />
+              )}
+            </FormControl>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+            >
+              Sign up
+            </Button>
+          </Box>
+          <Divider>
+            <Typography sx={{ color: 'text.secondary' }}>or</Typography>
+          </Divider>
+          <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+            <GoogleLoginButton/>
+            <Button
+              fullWidth
+              variant="outlined"
+              onClick={() => alert('Sign up with Facebook')}
+              startIcon={<FacebookIcon />}
+            >
+              Sign up with Facebook
+            </Button>
+            <Typography sx={{ textAlign: 'center' }}>
+              Already have an account?{' '}
+              <Link
+                component='button'
+                variant="body2"
+                sx={{ alignSelf: 'center' }}
+                onClick={() => navigate('/login')}
+              >
+                Sign in
+              </Link>
+            </Typography>
+          </Box>
+        </Card>
+      </SignUpContainer>
+    </AppTheme>
+  );
 }
