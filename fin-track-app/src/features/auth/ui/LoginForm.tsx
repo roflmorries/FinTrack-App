70bb2572408f57ed5ca1af84dcfa2b77ea41b8f9
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-07-13 18:52:38 +0300

    delete legacy code

--- fin-track-app/src/features/auth/ui/LoginForm.tsx
+++ fin-track-app/src/features/auth/ui/LoginForm.tsx
@@ -1,121 +1,3 @@
-// import { Button, Form, Input } from "antd"
-// import GoogleLoginButton from "./GoogleLoginButton"
-// import { useAppDispatch, useAppSelector } from "../../../shared/lib/hooks/redux/reduxTypes"
-// import { loginUser } from "../../../entities/user/model/userThunks";
-// import { useNavigate } from "react-router-dom";
-// import { useEffect } from "react";
-// import styled from "styled-components";
-// import { browserLocalPersistence, browserSessionPersistence, setPersistence } from "firebase/auth";
-// import { auth } from "../../../shared/config/firebase";
-
-// const CustomForm = styled.div`
-//   width: 35%;
-//   margin: 0 auto;
-//   display: flex;
-//   flex-direction: column;
-//   border: 1px solid #a100ff;
-//   min-height: 70vh;
-//   align-items: center;
-  
-
-//   .ant-form {
-//     width: 90%;
-//   }
-// `
-
-// const HeaderForm = styled.div`
-//   img {
-//     width: 100px;
-//     height: 100px;
-//   }
-//   h3 {
-//     font-size: 20px;
-//     margin: 0;
-//   }
-//   p {
-//     margin: 0;
-//     font-size: 14px;
-//     font-weight: 300;
-//   }
-// `
-
-// type Props = {}
-
-// export default function LoginForm({}: Props) {
-//   const dispatch = useAppDispatch();
-//   const navigate = useNavigate();
-//   const { isAuth, error} = useAppSelector((state) => state.user)
-
-//   useEffect(() => {
-//     if (isAuth) {
-//       navigate('/dashboard')
-//     }
-//   }, [isAuth, navigate]);
-
-//   const handleLoginForm = async (values: {email: string; password: string; rememberMe: boolean}) => {
-//     try {
-//       await setPersistence(
-//         auth, values.rememberMe ? browserLocalPersistence : browserSessionPersistence
-//       );
-//       await dispatch(loginUser(values))
-//     } catch(error) {
-//       console.error('Login error:', error)
-//     }
-//   }
-
-//   return (
-//     <CustomForm>
-
-//       <HeaderForm>
-//         <img src="/icon.png"></img>
-//         <h3>Welcome to FinTrack</h3>
-//         <p>Log in using the form below</p>
-//       </HeaderForm>
-
-//     {error && <div>Ошибка: {error}</div>}
-//     <Form
-//     name='Login'
-//     onFinish={handleLoginForm}
-//     layout="vertical"
-//     >
-
-//       <Form.Item
-//       label='Email'
-//       name='email'
-//       rules={[
-//         { required: true, message: 'Введите email' },
-//         { type: 'email', message: 'Некорректный email' },
-//       ]}
-//       >
-//         <Input placeholder="Введите Email" />
-//       </Form.Item>
-
-//       <Form.Item
-//       label='Password'
-//       name='password'
-//       rules={[
-//         { required: true, message: 'Введите email' },
-//       ]}
-//       >
-//         <Input placeholder="Введите пароль"/>
-//       </Form.Item>
-
-//       <Form.Item
-//       name='rememberMe'
-//       valuePropName="checked"
-//       >
-//         <Input type="checkbox"/>Remember Me
-//       </Form.Item>
-//       <Button type="primary" htmlType="submit">Войти</Button>
-//     </Form>
-//     <GoogleLoginButton/>
-//     <Button onClick={() => navigate('/reset-password')}>Forget Password?</Button>
-//     </CustomForm>
-//   )
-// }
-
-
-
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
@@ -186,11 +68,11 @@ const SignInContainer = styled(Stack)(({ theme }) => ({
 }));
 
 export default function LoginForm(props: { disableCustomTheme?: boolean }) {
-  const [emailError, setEmailError] = useState(false);
-  const [emailErrorMessage, setEmailErrorMessage] = useState('');
-  const [passwordError, setPasswordError] = useState(false);
-  const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
-  const [open, setOpen] = useState(false);
+  // const [emailError, setEmailError] = useState(false);
+  // const [emailErrorMessage, setEmailErrorMessage] = useState('');
+  // const [passwordError, setPasswordError] = useState(false);
+  // const [passwordErrorMessage, setPasswordErrorMessage] = useState('');
+  // const [open, setOpen] = useState(false);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { isAuth, error} = useAppSelector((state) => state.user)
@@ -289,10 +171,10 @@ export default function LoginForm(props: { disableCustomTheme?: boolean }) {
                 required
                 fullWidth
                 variant="outlined"
-                color={emailError ? 'error' : 'primary'}
+                // color={emailError ? 'error' : 'primary'}
                 sx={{
                   '& .MuiOutlinedInput-root': {
-                    borderRadius: '16px', // твой радиус
+                    borderRadius: '16px',
                   }
                 }}
               />
