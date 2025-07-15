c00771b2f357e4ba59688fcda621c8355669de53
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-07-15 21:20:15 +0300

    code improvements

--- fin-track-app/src/widgets/Dashboard/MonthlyBudgetWidget/MonthlyBudgetWidget.tsx
+++ fin-track-app/src/widgets/Dashboard/MonthlyBudgetWidget/MonthlyBudgetWidget.tsx
@@ -1,17 +1,15 @@
-import { useAppDispatch, useAppSelector } from '../../../shared/lib/hooks/redux/reduxTypes'
+import { useAppSelector } from '../../../shared/lib/hooks/redux/reduxTypes'
 import { selectMonthlyBudget } from '../../../entities/user/selectors/selectMonthlyBudget'
 import MonthlyBudgetForm from '../../../features/auth/ui/MonthlyBudgetForm';
 import { selectMonthlyExpenses } from '../../../entities/user/selectors/selectMonthlyExpenses';
 import Box from '@mui/material/Box';
 import { Typography } from 'antd';
 import { PieChart, Pie, Cell } from 'recharts';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import Button from '@mui/material/Button';
-import { addNotification } from '../../../entities/notifications/notificationThunk';
-import { selectAllNotifications } from '../../../entities/notifications/notificationSelectors';
 import { useBudgetNotifications } from '../../../shared/lib/hooks/useBudgetNotification';
 
 const COLORS = ['#0088FE', '#FF8042'];
@@ -19,9 +17,7 @@ const COLORS = ['#0088FE', '#FF8042'];
 export default function MonthlyBudgetWidget() {
   const budget = useAppSelector(selectMonthlyBudget);
   const expenses = useAppSelector(selectMonthlyExpenses);
-  const dispatch = useAppDispatch();
   const userId = useAppSelector(state => state.user.currentUser?.uid);
-  const notifications = useAppSelector(selectAllNotifications);
   const [open, setOpen] = useState(false);
 
   const spent = expenses;
@@ -72,7 +68,6 @@ export default function MonthlyBudgetWidget() {
             Monthly budget
         </text>
       </PieChart>
-      {/* ✅ ИСПРАВЛЕНО: показываем реальные цифры */}
       <Typography>Потрачено: {spent} / {budget || 0}</Typography>
       <Typography style={{ color: left < 0 ? '#ff4444' : '#fff' }}>
         {left >= 0 ? `Осталось: ${left}` : `Превышено на: ${Math.abs(left)}`}
