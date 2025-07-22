b626175d47f22e7ff5250cde003b32568c097556
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-07-22 21:22:30 +0300

    code improvements

--- fin-track-backend/src/config/cloudinaryConfig.ts
+++ fin-track-backend/src/config/cloudinaryConfig.ts
@@ -1,9 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary';
+import 'dotenv/config'
 
 cloudinary.config({
-  cloud_name: 'dqxkpzs25',
-  api_key: '159916394897579',
-  api_secret: 'pwiqCBD_FypJApO8TEVOwx-NHCg',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
