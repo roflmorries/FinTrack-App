d037606578766b7197a3cee5cdc91fb3950aa6fd
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-08-07 20:37:48 +0300

    delete legacy code

--- fin-track-app/src/features/auth/ui/LoginForm.tsx
+++ fin-track-app/src/features/auth/ui/LoginForm.tsx
@@ -106,32 +106,6 @@ export default function LoginForm(props: { disableCustomTheme?: boolean }) {
 
 
 
-  // const validateInputs = () => {
-  //   const email = document.getElementById('email') as HTMLInputElement;
-  //   const password = document.getElementById('password') as HTMLInputElement;
-
-  //   let isValid = true;
-
-  //   if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
-  //     setEmailError(true);
-  //     setEmailErrorMessage('Please enter a valid email address.');
-  //     isValid = false;
-  //   } else {
-  //     setEmailError(false);
-  //     setEmailErrorMessage('');
-  //   }
-
-  //   if (!password.value || password.value.length < 6) {
-  //     setPasswordError(true);
-  //     setPasswordErrorMessage('Password must be at least 6 characters long.');
-  //     isValid = false;
-  //   } else {
-  //     setPasswordError(false);
-  //     setPasswordErrorMessage('');
-  //   }
-
-  //   return isValid;
-  // };
 
   return (
     <AppTheme {...props}>
@@ -193,7 +167,7 @@ export default function LoginForm(props: { disableCustomTheme?: boolean }) {
                 color={error ? 'error' : 'primary'}
                 sx={{
                   '& .MuiOutlinedInput-root': {
-                    borderRadius: '16px', // твой радиус
+                    borderRadius: '16px',
                   }
                 }}
               />
