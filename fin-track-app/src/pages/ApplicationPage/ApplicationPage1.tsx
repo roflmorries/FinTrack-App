87010856939abfa907dce88126adb3a79e29d53b
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-08-07 19:56:25 +0300

    delete legacy code

--- fin-track-app/src/pages/ApplicationPage/ApplicationPage1.tsx
+++ fin-track-app/src/pages/ApplicationPage/ApplicationPage1.tsx
@@ -1,41 +1,40 @@
-import Sidebar from "../../widgets/Sidebar/Sidebar";
-import styled from 'styled-components';
-import { Layout } from 'antd';
-import { Outlet, useLocation } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
+// import Sidebar from "../../widgets/Sidebar/Sidebar";
+// import styled from 'styled-components';
+// import { Outlet, useLocation } from "react-router-dom";
+// import { AnimatePresence, motion } from "framer-motion";
 
-const { Content, Sider } = Layout;
+// const { Content, Sider } = Layout;
 
-const CustomLayout = styled(Layout)`
-  .ant-layout {
-    height: 100vh;
-  }
-`;
+// const CustomLayout = styled(Layout)`
+//   .ant-layout {
+//     height: 100vh;
+//   }
+// `;
 
-const fadeTransition = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: { opacity: 0 },
-  transition: { duration: 0.35 }
-};
+// const fadeTransition = {
+//   initial: { opacity: 0 },
+//   animate: { opacity: 1 },
+//   exit: { opacity: 0 },
+//   transition: { duration: 0.35 }
+// };
 
-export default function ApplicationPage() {
-  const location = useLocation();
+// export default function ApplicationPage() {
+//   const location = useLocation();
 
-  return (
-    <CustomLayout>
-      <Sider width={350} style={{ background: "#262626" }}>
-        <Sidebar/>
-      </Sider>
-      <Layout style={{ background: "#262626" }}>
-        <Content>
-          <AnimatePresence mode="wait">
-            <motion.div key={location.pathname} {...fadeTransition}>
-              <Outlet />
-            </motion.div>
-          </AnimatePresence>
-        </Content>
-      </Layout>
-    </CustomLayout>
-  );
-}
+//   return (
+//     <CustomLayout>
+//       <Sider width={350} style={{ background: "#262626" }}>
+//         <Sidebar/>
+//       </Sider>
+//       <Layout style={{ background: "#262626" }}>
+//         <Content>
+//           <AnimatePresence mode="wait">
+//             <motion.div key={location.pathname} {...fadeTransition}>
+//               <Outlet />
+//             </motion.div>
+//           </AnimatePresence>
+//         </Content>
+//       </Layout>
+//     </CustomLayout>
+//   );
+// }
