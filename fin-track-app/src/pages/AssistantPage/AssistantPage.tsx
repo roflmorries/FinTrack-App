20a5551021dca2674147df77e158cabeae82bc98
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-08-07 19:56:55 +0300

    delete legacy code

--- fin-track-app/src/pages/AssistantPage/AssistantPage.tsx
+++ fin-track-app/src/pages/AssistantPage/AssistantPage.tsx
@@ -3,17 +3,6 @@ import { lazy, Suspense } from "react";
 const AiAssistantChat = lazy(() => import ("../../features/ai-assistant/AiAssistantChat"))
 
 
-// const Layout = styled.div`
-//   background-color: #141414;
-//   height: 96%;
-//   border-radius: 24px;
-//   padding: 1px;
-//   margin: 20px;
-//   p {
-//     color: white
-//   }
-// `;
-
 export default function AssistantPage() {
   return (
     <Box sx={{ width: '100%', height: '91vh' }}>
