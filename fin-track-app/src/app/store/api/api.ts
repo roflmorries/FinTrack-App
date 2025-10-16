7354729d0c38eea0562625e290b0e28afd445297
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-16 19:44:33 +0300

    add prepare headers

--- fin-track-app/src/app/store/api/api.ts
+++ fin-track-app/src/app/store/api/api.ts
@@ -1,10 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { API_URL } from '../../../shared/config/config'
+import { auth } from '../../../shared/config/firebase'
+import { getIdToken } from 'firebase/auth';
 
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
+  baseQuery: fetchBaseQuery({ 
+    baseUrl: API_URL,
+    prepareHeaders: async headers => {
+      const user = auth.currentUser;
+      if (user) {
+        const token = await getIdToken(user);
+        headers.set('Authorization', `Bearer ${token}`)
+      }
+      return headers;
+    }
+   }),
   tagTypes: ['Transaction', 'Category', 'Goal', 'Notification', 'User'],
   endpoints: () => ({})
 })
