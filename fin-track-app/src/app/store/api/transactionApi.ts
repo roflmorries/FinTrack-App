14460815766b2c7e38e5f749724d300d0fd3fb23
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-17 19:16:13 +0300

    add fetch transactions

--- fin-track-app/src/app/store/api/transactionApi.ts
+++ fin-track-app/src/app/store/api/transactionApi.ts
@@ -1,8 +1,13 @@
+import { Transaction } from "../../../entities/transactions/model/types";
+import { API_URL } from "../../../shared/config/config";
 import { api } from "./api";
 
+const API_URL_TRANSACTIONS = `${API_URL}/transactions`;
 
 export const transactionApi = api.injectEndpoints({
   endpoints: builder => ({
-    fetchTransactions: builder.query
+    fetchTransactions: builder.query<Transaction[], string>({
+      query: userId => `${API_URL_TRANSACTIONS}?userId=${userId}`,
+    })
   })
 })
