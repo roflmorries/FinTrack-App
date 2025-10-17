eafd27e8f776bf075e269bddcfebbbf757884f74
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-17 19:16:00 +0300

    add query to store

--- fin-track-app/src/app/store/store.ts
+++ fin-track-app/src/app/store/store.ts
@@ -4,6 +4,7 @@ import transactionReducer from '../../entities/transactions/model/transactionSli
 import categoryReducer from '../../entities/categories/model/categorySlice';
 import goalReducer from '../../entities/fin-goals/goalSlice';
 import notificationReducer from '../../entities/notifications/notificationSlice'
+import { api } from "./api/api";
 
 export const store = configureStore({
   reducer: {
@@ -11,8 +12,11 @@ export const store = configureStore({
     transaction: transactionReducer,
     category: categoryReducer,
     goal: goalReducer,
-    notification: notificationReducer
+    notification: notificationReducer,
+    [api.reducerPath]: api.reducer
   },
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
