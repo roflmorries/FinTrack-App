5e668f8fbdfaa20d18c4543a9474ec971a9dce6d
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-18 19:12:30 +0300

    feat(api): add transaction API endpoints for RTK Query

--- fin-track-app/src/app/store/api/transactionApi.ts
+++ fin-track-app/src/app/store/api/transactionApi.ts
@@ -8,8 +8,43 @@ export const transactionApi = api.injectEndpoints({
   endpoints: builder => ({
     fetchTransactions: builder.query<Transaction[], string>({
       query: (userId: string) => `${API_URL_TRANSACTIONS}?userId=${userId}`,
+      providesTags: (result) =>
+        result
+          ? [
+              ...result.map(({ id }) => ({ type: 'Transaction' as const, id })),
+              { type: 'Transaction', id: 'LIST' },
+            ]
+          : [{ type: 'Transaction', id: 'LIST' }],
+    }),
+    createTransaction: builder.mutation<Transaction, Omit<Transaction, 'id'>>({
+      query: (transaction) => ({
+        url: API_URL_TRANSACTIONS,
+        method: 'POST',
+        body: transaction
+      }),
+      invalidatesTags: [{ type: 'Transaction', id: 'LIST' }]
+    }),
+    updateTransaction: builder.mutation<Transaction, { id: string, changes: Partial<Transaction> }>({
+      query: ({ id, changes }) => ({
+        url: `${API_URL_TRANSACTIONS}/${id}`,
+        method: 'PUT',
+        body: changes
+      }),
+      invalidatesTags: (result, error, {id}) => [{ type: 'Transaction', id }]
+    }),
+    deleteTransaction: builder.mutation<string, string>({
+      query: id => ({
+        url: `${API_URL_TRANSACTIONS}/${id}`,
+        method: 'DELETE'
+      }),
+      invalidatesTags: (result, error, id) => [{ type: 'Transaction', id }]
     })
   })
 })
 
-export const { useFetchTransactionsQuery } = transactionApi
+export const {
+  useFetchTransactionsQuery,
+  useCreateTransactionMutation,
+  useUpdateTransactionMutation,
+  useDeleteTransactionMutation,
+ } = transactionApi
