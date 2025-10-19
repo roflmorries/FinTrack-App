0bdccb8fb6d2602f881bc8a0dac1cc56f768017b
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-19 21:01:26 +0300

    delete legacy code

--- fin-track-app/src/entities/categories/model/defaultCategories.ts
+++
@@ -1,9 +0,0 @@
-import { Category } from "./types";
-
-export const defaultCategories: Omit<Category, "id" | "userId">[] = [
-  {name: 'Salary', color: '#4CAF50'},
-  {name: 'Food', color: '#FF9800'},
-  {name: 'Transport', color: '	#2196F3'},
-  {name: 'Entertainment', color: '	#9C27B0'},
-  {name: 'Goals', color: '	#FFC106'}
-]
