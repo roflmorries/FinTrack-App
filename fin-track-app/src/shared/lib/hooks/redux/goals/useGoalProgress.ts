19bae21198aa49ce289c383bbcc61230f1c65195
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-20 19:56:25 +0300

    feat(goals) add custom hook for calc goal progress

---
+++ fin-track-app/src/shared/lib/hooks/redux/goals/useGoalProgress.ts
@@ -0,0 +1,26 @@
+import { useMemo } from "react";
+import { useFetchTransactionsQuery } from "../../../../../app/store/api/transactionApi"
+
+/**
+ * Hook for calculation current progress toward a fin-goal
+ * @param userId 
+ * @param goalId 
+ * @returns Amount of savings for the goals (income minus expenses)
+ */
+
+
+export const useGoalProgress = (userId: string | undefined, goalId: string | undefined) => {
+  const { data: transactions = [] } = useFetchTransactionsQuery(userId || '', { skip: !userId || !goalId });
+
+  return useMemo(() => {
+    if (!goalId) return;
+
+    return transactions
+      .filter(tx => tx.goalId === goalId)
+      .reduce((acc, tx) => {
+        if (tx.type === 'income') return acc + tx.amount;
+        if (tx.type === 'expense') return acc - tx.amount;
+        return acc;
+      }, 0)
+  }, [transactions, goalId])
+}
