c6136669c9b79ea30c35c3c022891251899460c9
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-20 11:58:37 +0300

    feat(goals) create CRUD endpoints for RTK Query

---
+++ fin-track-app/src/app/store/api/goalsApi.ts
@@ -0,0 +1,42 @@
+import { Goal } from "../../../entities/fin-goals/types";
+import { API_URL_GOALS } from "../../../shared/config/config";
+import { api } from "./api";
+
+
+export const goalsApi = api.injectEndpoints({
+  endpoints: builder => ({
+    getGoals: builder.query<Goal[], string>({
+      query: (userId: string) => `${API_URL_GOALS}?userId=${userId}`,
+      providesTags: (result) =>
+        result
+          ? [
+            ...result.map(({ id }) => ({ type: 'Goal' as const, id })),
+            { type: 'Goal', id: 'LIST' },
+          ]
+          : [{ type: 'Goal', id: 'LIST' }],
+    }),
+    createGoal: builder.mutation<Goal, Omit<Goal, 'id'>>({
+      query: (goal) => ({
+        url: API_URL_GOALS,
+        method: 'POST',
+        body: goal
+      }),
+      invalidatesTags: [{ type: 'Goal', id: 'LIST' }]
+    }),
+    updateGoal: builder.mutation<Goal, { id: string, changes: Partial<Goal> }>({
+      query: ({ id, changes }) => ({
+        url: `${API_URL_GOALS}/${id}`,
+        method: 'PUT',
+        body: changes
+      }),
+      invalidatesTags: (result, error, { id }) => [{ type: 'Goal', id }]
+    }),
+    deleteGoal: builder.mutation<string, string>({
+      query: id => ({
+        url: `${API_URL_GOALS}/${id}`,
+        method: 'DELETE'
+      }),
+      invalidatesTags: (result, error, id) => [{ type: 'Goal', id }]
+    }),
+  })
+})
