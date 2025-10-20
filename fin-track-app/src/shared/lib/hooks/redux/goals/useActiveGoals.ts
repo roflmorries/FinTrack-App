ddce9e9bf0d35fc860d78af58a35e6846311df7b
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-20 19:57:05 +0300

    feat(goals) add custom hood for getting active goals

---
+++ fin-track-app/src/shared/lib/hooks/redux/goals/useActiveGoals.ts
@@ -0,0 +1,33 @@
+import { useMemo } from "react";
+import { useGetGoalsQuery } from "../../../../../app/store/api/goalsApi"
+import { useFetchTransactionsQuery } from "../../../../../app/store/api/transactionApi";
+import dayjs from "dayjs";
+
+/**
+ * Hook for getting active fin-goals (not expired & completed)
+ * @param userId 
+ * @returns array of active goals
+ */
+
+
+export const useActiveGoals = (userId: string | undefined) => {
+  const { data: goals = [] } = useGetGoalsQuery(userId || '', { skip: !userId });
+  const { data: transactions = [] } = useFetchTransactionsQuery(userId || '', { skip: !userId });
+
+  return useMemo(() => {
+    return goals.filter(goal => {
+      const sum = transactions
+        .filter(tx => tx.goalId === goal.id)
+        .reduce((acc, tx) => {
+          if (tx.type === 'income') return acc + tx.amount;
+          if (tx.type === 'expense') return acc - tx.amount;
+          return acc;
+        }, 0);
+
+        const isCompleted = sum >= goal.amount;
+        const isExpired = dayjs().isAfter(dayjs(goal.deadline));
+
+        return !isCompleted && !isExpired;
+    })
+  }, [goals, transactions])
+}
