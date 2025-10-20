7fa4c06feb5cfffba74178ba54d96d0002ac6902
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-20 21:31:12 +0300

    feat(notification) add CRUD endpoints for RTK Query

---
+++ fin-track-app/src/app/store/api/notificationApi.ts
@@ -0,0 +1,52 @@
+import { Notification } from "../../../entities/notifications/types";
+import { API_URL_NOTIFICATIONS } from "../../../shared/config/config";
+import { api } from "./api";
+
+
+export const notificationApi = api.injectEndpoints({
+  endpoints: builder => ({
+    getNotification: builder.query<Notification[], string>({
+          query: (userId: string) => `${API_URL_NOTIFICATIONS}?userId=${userId}`,
+          providesTags: (result) =>
+            result
+              ? [
+                ...result.map(({ id }) => ({ type: 'Goal' as const, id })),
+                { type: 'Notification', id: 'LIST' },
+              ]
+              : [{ type: 'Notification', id: 'LIST' }],
+        }),
+        addNotification: builder.mutation<Notification, Omit<Notification, 'id' | 'date'>>({
+          query: (notification) => ({
+            url: API_URL_NOTIFICATIONS,
+            method: 'POST',
+            body: notification
+          }),
+          invalidatesTags: [{ type: 'Notification', id: 'LIST' }]
+        }),
+        markAsRead: builder.mutation<Notification, string>({
+          query: (id) => ({
+            url: `${API_URL_NOTIFICATIONS}/${id}`,
+            method: 'PUT',
+            body: {read: true}
+          }),
+          invalidatesTags: (result, error, id) => [{ type: 'Notification', id }]
+        }),
+        deleteNotification: builder.mutation<void, string>({
+          query: id => ({
+            url: `${API_URL_NOTIFICATIONS}/${id}`,
+            method: 'DELETE'
+          }),
+          invalidatesTags: (result, error, id) => [
+            { type: 'Notification', id },
+            { type: 'Notification', id: 'LIST' }
+          ]
+        }),
+  })
+});
+
+export const {
+  useGetNotificationQuery,
+  useAddNotificationMutation,
+  useMarkAsReadMutation,
+  useDeleteNotificationMutation
+} = notificationApi;
