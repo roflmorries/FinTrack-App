93a9389d8a5ccafd10f1c1c4a9e34dc6281b395d
roflmorries <99510874+roflmorries@users.noreply.github.com>
2025-10-22 16:37:24 +0300

    refactor(notifications) refactor code to use RTK Query

--- fin-track-app/src/widgets/Notifications/AlertNotification.tsx
+++ fin-track-app/src/widgets/Notifications/AlertNotification.tsx
@@ -1,45 +1,58 @@
 import { useEffect, useRef } from 'react';
 import { toast } from 'react-toastify';
-import { useAppSelector, useAppDispatch } from '../../shared/lib/hooks/redux/reduxTypes';
-import { selectAllNotifications } from '../../entities/notifications/notificationSelectors';
-import { markAsRead } from '../../entities/notifications/notificationThunk';
+import { useAppSelector } from '../../shared/lib/hooks/redux/reduxTypes';
+import { useGetNotificationsQuery, useMarkAsReadMutation } from '../../app/store/api/notificationApi';
 
 export default function AlertNotification() {
-  const notifications = useAppSelector(selectAllNotifications);
-  const dispatch = useAppDispatch();
+  const userId = useAppSelector(state => state.user.currentUser?.uid)
+  const { data: notifications = [], error, isLoading } = useGetNotificationsQuery(userId || '', { skip: !userId });
+  const [markAsRead] = useMarkAsReadMutation();
   const shownNotifications = useRef(new Set<string>());
+
+  
+  console.log('Notifications data:', notifications);
+  console.log('Notifications error:', error);
+  console.log('Notifications loading:', isLoading);
 
   useEffect(() => {
     notifications.forEach(notification => {
       if (!notification.read && !shownNotifications.current.has(notification.id)) {
-        
+
+        const handleMarkAsRead = async () => {
+          try {
+            await markAsRead(notification.id).unwrap();
+          } catch (error) {
+            console.error('Failed to mark notification as read', error)
+          }
+        }
+
         switch (notification.severity) {
           case 'error':
             toast.error(notification.message, {
               toastId: notification.id,
               autoClose: 6000,
-              onClose: () => dispatch(markAsRead(notification.id))
+              onClose: handleMarkAsRead
             });
             break;
           case 'warning':
             toast.warn(notification.message, {
               toastId: notification.id,
               autoClose: 5000,
-              onClose: () => dispatch(markAsRead(notification.id))
+              onClose: handleMarkAsRead
             });
             break;
           default:
             toast.info(notification.message, {
               toastId: notification.id,
               autoClose: 4000,
-              onClose: () => dispatch(markAsRead(notification.id))
+              onClose: handleMarkAsRead
             });
         }
-        
+
         shownNotifications.current.add(notification.id);
       }
     });
-  }, [notifications, dispatch]);
+  }, [notifications, markAsRead]);
 
   return null;
 }
